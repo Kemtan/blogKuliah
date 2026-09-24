478b46600d1af4f4468ceacac0a4be8750389438
kemtan <waluyatristan@gmail.com>
2026-09-24 11:03:44 +0700

    newline and latex support in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,46 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
+
+// markdown
+import { defineConfig } from "astro/config";
+import { unified } from "@astrojs/markdown-remark";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import remarkBreaks from "remark-breaks";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+    site: "https://example.com",
+    integrations: [mdx(), sitemap()],
+    markdown: {
+        remarkPlugins: [remarkMath, remarkBreaks],
+        rehypePlugins: [rehypeKatex],
+    },
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: "Atkinson",
+            cssVariable: "--font-atkinson",
+            fallbacks: ["sans-serif"],
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/atkinson-regular.woff"],
+                        weight: 400,
+                        style: "normal",
+                        display: "swap",
+                    },
+                    {
+                        src: ["./src/assets/fonts/atkinson-bold.woff"],
+                        weight: 700,
+                        style: "normal",
+                        display: "swap",
+                    },
+                ],
+            },
+        },
+    ],
 });
